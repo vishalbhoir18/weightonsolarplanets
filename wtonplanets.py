ec96f6b0cfd1f64d9b127a95dd3b5cc090214926
vishalbhoir18 <67859075+vishalbhoir18@users.noreply.github.com>
2024-08-27 17:29:24 +0530

    Update wtonplanets.py

--- wtonplanets.py
+++ wtonplanets.py
@@ -53,3 +53,18 @@ elif planet == "Pluto [dwarf planet]":
 
 st.write("Note: The weight has been calculated based on the gravitational pull on each planet/moon relative to Earth.")
 st.write("_Reference: https://www.exploratorium.edu_")
+st.write("The App is developed by Vishal Bhoir visit for more info: _https://linktr.ee/thebioway_")
+
+from st_social_media_links import SocialMediaIcons
+
+social_media_links = [
+    "https://www.facebook.com/thebiowayonline",
+    "https://www.youtube.com/c/VishalBhoirTheBioway",
+    "https://www.instagram.com/the_bioway/",
+    "https://twitter.com/The_Bioway",
+    "https://github.com/vishalbhoir18",
+]
+
+social_media_icons = SocialMediaIcons(social_media_links)
+
+social_media_icons.render()
